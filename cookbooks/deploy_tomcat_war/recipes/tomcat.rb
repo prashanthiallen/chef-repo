730527b2cf9b1dd4cc8a585348c66ba344dd3200
prashanthiallen <s.prashanthiallen@gmail.com>
2017-06-07 09:12:44 +0530

    Update tomcat.rb

--- cookbooks/deploy_tomcat_war/recipes/tomcat.rb
+++ cookbooks/deploy_tomcat_war/recipes/tomcat.rb
@@ -1,31 +1,3 @@
-=begin
-
-install java from debain file
-install tomcat from tar file
-deploy war file
-start tomcat service
-
-remote_file "/home/#{node['deploy_tomcat_war']['user']}/jre-8u131-linux-i586.tar.gz" do
-  source 'https://s3.amazonaws.com/thecamion-paperclip-dev/jre-8u131-linux-i586.tar.gz'
-  owner node['deploy_tomcat_war']['user']
-  group node['deploy_tomcat_war']['group']
-  mode '0755'
-  action :create
-end
-
-# remote_file '/tmp/zonemig.zip' do
-#   source 'https://s3.amazonaws.com/thecamion-paperclip-dev/jre-8u131-linux-i586.tar.gz'
-# end
-
-#cd ~
-#tar -xzf *.tar.gz /opt/
-
-execute 'java-extract' do
-  command "tar -xzf /home/#{node['deploy_tomcat_war']['user']}/jre-8u131-linux-i586.tar.gz"
-  cwd '/opt'
-end
-
-=end
 
 package 'java'
 remote_file "/home/#{node['deploy_tomcat_war']['user']}/apache-tomcat-8.5.15.tar.gz" do
